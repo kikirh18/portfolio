852b3e9fc6490543a76635eaff5448ec38d21f52
kikirh <88475333+kikirh18@users.noreply.github.com>
2021-10-17 15:09:28 +0700

    update alert.js

--- asset/js/main/alert.js
+++ asset/js/main/alert.js
@@ -4,7 +4,7 @@ function showAlert(){
         text: 'thanks for download my CV',
     }).then(function (result) {
 if (true) {
-    window.open("https://drive.google.com/file/d/1gCBByAmBj4YEzGhg72P1MAn-rpnp4d1c/view?usp=sharing", '_blank');
+    window.open("https://drive.google.com/file/d/1uV4PGvyfRs9WYjnNYTxgqQ-1PKIVTewe/view?usp=sharing", '_blank');
 }
 })
 
